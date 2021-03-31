63597467fb07a9358f6a80d2e37a2a4495dd1a86
liam <liam.lee@email.com>
2021-03-31 10:07:37 +0800

    add edge

--- config.def.h
+++ config.def.h
@@ -72,6 +72,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *alacritty[] = { "alacritty", NULL };
+static const char *edge[]= { "microsoft-edge-dev", NULL };
 static const char *slockcmd[] = { "slock", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
@@ -82,6 +83,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = slockcmd} },
 	{ MODKEY|ShiftMask,             XK_a,      spawn,          {.v = alacritty} },
+	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = edge} },
 	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
